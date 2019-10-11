8ad7b866af943226cdbd121b9e3f04e271c2a791
Dabin2560 <hdb7759@163.com>
2019-10-11 16:34:08 +0800

    初始化

---
+++ pages/hotel/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="box">hotel</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="box">post</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
